53455369bdb09bd24e6b3a443cee7e447c4d12da
Scott Torborg <storborg@gmail.com>
2016-02-08 16:59:01 -0800

    Extract color mapping functions into a separate file

---
+++ src/colormap.h
@@ -0,0 +1,5 @@
+#pragma once
+
+void render_complex(png_byte *ptr, fftw_complex val);
+
+void print_scale_stats();
--- src/renderfall.c
+++ src/renderfall.c
@@ -13,53 +13,7 @@
 
 #include "formats.h"
 #include "window.h"
-
-double maxdb = 0;
-double mindb = FLT_MAX;
-
-int32_t maxval = 0;
-int32_t minval = INT32_MAX;
-
-void scale_log(png_byte *ptr, double val) {
-    // Make a monochromatic purple output for now.
-    double db = log10(val);
-    if (db > maxdb) maxdb = db;
-    if (db < mindb) mindb = db;
-
-    // Kind of arbitrarily picked.
-    int32_t v = (int32_t) ((db * 85.0f) + 50.0f);
-
-    if (v > maxval) maxval = v;
-    if (v < minval) minval = v;
-
-    v = 255 - v;
-
-    if (v > 255) v = 255;
-    if (v < 0) v = 0;
-
-    ptr[0] = (uint8_t) v;
-    ptr[1] = (uint8_t) v;
-    ptr[2] = (uint8_t) v;
-}
-
-void scale_linear(png_byte *ptr, double val) {
-    int32_t v = (int32_t) (val * 100.0f);
-    if (v > maxval) maxval = v;
-    if (v < minval) minval = v;
-
-    if (v > 255) v = 255;
-    if (v < 0) v = 0;
-
-    // Make a monochromatic purple output for now.
-    ptr[0] = (uint8_t) v;
-    ptr[1] = 0;
-    ptr[2] = (uint8_t) v;
-}
-
-void render_complex(png_byte *ptr, fftw_complex val) {
-    double mag = hypot(val[0], val[1]);
-    scale_log(ptr, mag);
-}
+#include "colormap.h"
 
 void waterfall(png_structp png_ptr, FILE* fp,
                uint32_t w, uint32_t h, read_samples_fn reader) {
@@ -287,11 +241,7 @@ int main(int argc, char *argv[]) {
     fclose(writefp);
     fclose(readfp);
 
-    printf("Max dB: %f\n", maxdb);
-    printf("Min dB: %f\n", mindb);
-
-    printf("Max value: %d\n", maxval);
-    printf("Min value: %d\n", minval);
+    print_scale_stats();
 
     return EXIT_SUCCESS;
 }
