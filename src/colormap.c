d4696f3fdb9ed5ada0fa88ff35ecb45f3526ad8c
Scott Torborg <storborg@gmail.com>
2016-06-25 20:56:32 -0700

    Add a hue-based palette for experimentation purposes

--- src/colormap.c
+++ src/colormap.c
@@ -11,6 +11,47 @@ double mindb = FLT_MAX;
 int32_t maxval = 0;
 int32_t minval = INT32_MAX;
 
+void hsv_to_rgb(double *r, double *g, double *b, double h, double s, double v) {
+    //fprintf(stderr, "hsv_to_rgb - hue %f / sat %f / val %f\n\n", h, s, v);
+    double c = 0.0, m = 0.0, x = 0.0;
+
+    h *= 360.0;
+
+    c = v * s;
+    x = c * (1.0 - fabs(fmod(h / 60.0, 2) - 1.0));
+    m = v - c;
+
+    if (h >= 0.0 && h < 60.0) {
+        *r = c + m;
+        *g = x + m;
+        *b = m;
+    } else if (h >= 60.0 && h < 120.0) {
+        *r = x + m;
+        *g = c + m;
+        *b = m;
+    } else if (h >= 120.0 && h < 180.0) {
+        *r = m;
+        *g = c + m;
+        *b = x + m;
+    } else if (h >= 180.0 && h < 240.0) {
+        *r = m;
+        *g = x + m;
+        *b = c + m;
+    } else if (h >= 240.0 && h < 300.0) {
+        *r = x + m;
+        *g = m;
+        *b = c + m;
+    } else if (h >= 300.0 && h < 360.0) {
+        *r = c + m;
+        *g = m;
+        *b = x + m;
+    } else {
+        *r = m;
+        *g = m;
+        *b = m;
+    }
+}
+
 void scale_log(png_byte *ptr, double val) {
     // Make a monochromatic black-on-white output for now.
     double db = log10(val);
@@ -31,6 +72,32 @@ void scale_log(png_byte *ptr, double val) {
     ptr[0] = (uint8_t) v;
     ptr[1] = (uint8_t) v;
     ptr[2] = (uint8_t) v;
+}
+
+void scale_log_hue(png_byte *ptr, double val) {
+    double h = 0.0, s = 0.0, v = 0.0;
+    double r, g, b;
+
+    double db = log10(val);
+    if (db > maxdb) maxdb = db;
+    if (db < mindb) mindb = db;
+
+    // Kind of arbitrarily picked.
+    h = (db + 4.0) / 4.0;
+    s = 1.0;
+    v = 1.0;
+
+    if (h > maxval) maxval = h;
+    if (h < minval) minval = h;
+
+    if (h > 1.0) h = 1.0;
+    if (h < 0.0) h = 0.0;
+
+    hsv_to_rgb(&r, &g, &b, h, s, v);
+
+    ptr[0] = (uint8_t) (255.0 * r);
+    ptr[1] = (uint8_t) (255.0 * g);
+    ptr[2] = (uint8_t) (255.0 * b);
 }
 
 void scale_linear(png_byte *ptr, double val) {
