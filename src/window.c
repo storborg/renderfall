5beab8b199afeb870c69ad88873800bf4fdcc4bd
Paul Wicks <pwicks86@gmail.com>
2016-06-27 14:45:24 -0700

    Add Blackman Harris and Hamming window functions

--- src/window.c
+++ src/window.c
@@ -66,6 +66,42 @@ window_t make_window_blackman(uint32_t n) {
     return win;
 }
 
+window_t make_window_hamming(uint32_t n) {
+    double *coeffs = (double*) malloc(n * sizeof(double));
+    double alpha = 0.54;
+    double beta = 0.46;
+    double constant = alpha - beta;
+
+    for (uint32_t k = 0; k < n; k++) {
+        coeffs[k] = constant * cos((2* M_PI * k) / (n - 1));
+    }
+
+    window_t win;
+    win.size = n;
+    win.coeffs = coeffs;
+    return win;
+}
+
+window_t make_window_blackman_harris(uint32_t n) {
+    double *coeffs = (double*) malloc(n * sizeof(double));
+    double a0 = 0.35875;
+    double a1 = 0.48829;
+    double a2 = 0.14128;
+    double a3 = 0.01168;
+
+    for (uint32_t k = 0; k < n; k++) {
+        coeffs[k] = (a0-a1) * cos((2 * M_PI * k) / (n -1)) +
+                    a2 * cos((4 * M_PI * k) / (n -1)) +
+                    a3 * cos((6 * M_PI * k) / (n -1));
+
+    }
+
+    window_t win;
+    win.size = n;
+    win.coeffs = coeffs;
+    return win;
+}
+
 void destroy_window(window_t win) {
     free(win.coeffs);
 }
